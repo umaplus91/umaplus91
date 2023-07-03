f1f7b0389e178150a9629df6de306dcf4b08bdfc
Ukh <137356503+umaplus91@users.noreply.github.com>
2023-07-03 19:44:21 -0400

    Add files via upload

--- App.java
+++ App.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 import org.testng.Assert;
@@ -12,16 +13,15 @@ import org.testng.Assert;
 public class App extends Core {
 	String url = "https://www.walmart.com/cp/smart-home/1229875";
 	String web = "https://www.google.com/";
-	String www = "https://www.dollartree.com/";
-	
+	String DollarTree = "https://www.dollartree.com/";
 
-	@AfterMethod
+	// @AfterMethod
 	public void closepage() throws InterruptedException {
 		Thread.sleep(4000);
 		driver.close();
 	}
 
-	@Test
+	// @Test
 	public void walmartiframe() throws InterruptedException {
 		Actions actions = new Actions(driver);
 		driver.get(url);
@@ -44,23 +44,22 @@ public class App extends Core {
 		driver.close();
 	}
 
-	//@Test
+	// @Test
 	public void googleAssert() throws InterruptedException {
 		driver.get(web);
 		Thread.sleep(2000);
 		String title = driver.findElement(By.xpath("//img[@alt='Google']")).getText();
 		System.out.println(title);
-		
-		//Equality check 
+
+		// Equality check
 		Assert.assertEquals(title, "gogle", "Page title is incorrect");
-		
 
 	}
 
-	//@Test
+	@Test
 	public void dollartreeActions() throws InterruptedException {
-		driver.get(www);
-		Thread.sleep(3000);
+		driver.get(DollarTree);
+		Thread.sleep(4000);
 
 		WebElement search = driver.findElement(By.xpath("(//span[@class='dropdown-toggle'])[5]"));
 		Thread.sleep(3000);
@@ -69,7 +68,64 @@ public class App extends Core {
 		Thread.sleep(3000);
 		driver.findElement(By.xpath("//*[@id=\"health-personal-care\"]/div/div[1]/ul/li[2]/a")).click();
 		Thread.sleep(5000);
-		driver.close();
-
+		driver.findElement(By.xpath("//a[@class='modal-close email-signup-modal-close']")).click();
+		Thread.sleep(1000);
+		JavascriptExecutor je = (JavascriptExecutor) driver;
+		je.executeScript("window.scrollBy(0,500)");
+		Thread.sleep(2000);
+		je.executeScript("window.scrollBy(0,-500)");
+		Thread.sleep(500);
+		WebElement sear = driver.findElement(By.id("regular-search-input-desktop"));
+		sear.sendKeys("Bounty");
+		Thread.sleep(2000);
+		sear.clear();
+		Thread.sleep(2000);
+		sear.sendKeys("Tide");
+		sear.sendKeys(Keys.ENTER);
+		Thread.sleep(4000);
+		driver.findElement(By.cssSelector("button[class='btn btn-default']")).click();
+		je.executeScript("window.scrollBy(0,300)");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("(//div[@class='image-badges-container image-badges-container-right'])[1]")).click();
+		Thread.sleep(2000);
+		je.executeScript("window.scrollBy(0,400)");
+		Thread.sleep(3000);
+		//Select more = new Select(driver.findElement(By.cssSelector("div[class='quantity-value']")));
+		//more.selectByValue("store-home-24");
+		driver.findElement(By.id("cc-prodDetailsAddToCart")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//a[@class='btn cc-button-primary continue-shopping-link']")).click();	
+		Thread.sleep(2000);
+		je.executeScript("window.scrollBy(0,-500)");
+		WebElement drinks = driver.findElement(By.xpath("(//span[@class='dropdown-toggle'])[6]"));
+		actions.moveToElement(drinks).build().perform();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("(//a[@href='/food-candy-drinks/drinks'])[3]")).click(); 
+		Thread.sleep(3000);
+		je.executeScript("window.scrollBy(0,800)");
+		driver.findElement(By.xpath("(//p[@class='bi'])[8]")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.cssSelector("a[href='/arizona-iced-tea-with-lemon-338oz-cans/850739']")).click();
+		Thread.sleep(2000);
+		je.executeScript("window.scrollBy(0,400)");
+		Thread.sleep(2000);
+		driver.findElement(By.cssSelector("div[id='quantity-value']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.cssSelector("div[id='store-home-24']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.id("cc-prodDetailsAddToCart")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//a[@class='btn cc-button-primary continue-shopping-link']")).click();	
+		Thread.sleep(2000);
+		je.executeScript("window.scrollBy(0,900)");
+		Thread.sleep(2000);
+		//review (shadow root)
+		//driver.findElement(By.xpath("/html/body/div[4]/main/div[1]/div[3]/div/div/div[5]/div[1]//div/div/div[2]/div[1]/div[3]/div/div[1]/div/div[4]")).click();
+		driver.findElement(By.cssSelector("a[class='links-section-item cart']")).click();
+		
+		
+   
+   
+		
 	}
 }
